5e127aead446f2d7ed8c7b7072b3fc1ecc9c89e2
GaneshKuklare <gskuklare123@gmail.com>
2025-01-18 19:40:49 +0530

    Updated Changes

--- src/src_main_Java/Test1.java
+++ src/src_main_Java/Test1.java
@@ -1,5 +1,11 @@
 package src_main_Java;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
+
 public class Test1 {
 
 	public static void main(String[] args) {
@@ -10,7 +16,18 @@ public class Test1 {
 		System.out.println("This is Ganesh Kuklare");
 
 		System.out.println("This is Ganesh Kuklare");
-
+		
+		WebDriver driver = new ChromeDriver();
+		driver.get("https://www.worldometers.info/world-population/");
+		driver.manage().window().maximize();
+		driver.manage().deleteAllCookies();
+		WebElement element = driver.findElement(By.xpath("//div[@class='maincounter-number']"));
+		String text = element.getText();
+		System.out.println("Text: " + text);
+		
+		
+		Actions action = new Actions(driver);
+		action.contextClick().perform();
 
 		
 		
